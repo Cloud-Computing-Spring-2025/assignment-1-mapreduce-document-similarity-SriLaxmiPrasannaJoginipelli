568da2e5708256fa64d4d4ccaf752c3155190d84
SriLaxmiPrasannaJoginipelli <srilurao99@gmail.com>
2025-02-14 16:58:54 +0000

    Initial Commit

--- src/main/java/com/example/DocumentSimilarityMapper.java
+++ src/main/java/com/example/DocumentSimilarityMapper.java
@@ -1,5 +1,25 @@
 package com.example;
 
-public class DocumentSimilarityMapper  {
-    
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class DocumentSimilarityMapper extends Mapper<Object, Text, Text, Text> {
+    private Text docId = new Text();
+    private Text word = new Text();
+
+    @Override
+    protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        
+        String[] parts = value.toString().split("\t", 2);
+        if (parts.length == 2) {
+            docId.set(parts[0]); // Document ID
+            StringTokenizer tokenizer = new StringTokenizer(parts[1]); 
+            while (tokenizer.hasMoreTokens()) {
+                word.set(tokenizer.nextToken()); 
+                context.write(docId, word);
+            }
+        }
+    }
 }
--- src/main/java/com/example/DocumentSimilarityReducer.java
+++ src/main/java/com/example/DocumentSimilarityReducer.java
@@ -1,5 +1,48 @@
 package com.example;
 
-public class DocumentSimilarityReducer {
-    
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+import java.io.IOException;
+import java.util.*;
+
+public class DocumentSimilarityReducer extends Reducer<Text, Text, Text, Text> {
+    private Map<String, Set<String>> documentWords = new HashMap<>();
+
+    @Override
+protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+    Set<String> words = new HashSet<>();
+    for (Text word : values) {
+        words.add(word.toString());
+    }
+    documentWords.put(key.toString(), words);
+    System.out.println("Reducer: Collected words for " + key.toString() + " -> " + words); // Debugging log
+}
+
+@Override
+protected void cleanup(Context context) throws IOException, InterruptedException {
+    System.out.println("Reducer: Starting similarity calculation"); // Debugging log
+    List<String> docList = new ArrayList<>(documentWords.keySet());
+    for (int i = 0; i < docList.size(); i++) {
+        for (int j = i + 1; j < docList.size(); j++) {
+            String doc1 = docList.get(i);
+            String doc2 = docList.get(j);
+            Set<String> words1 = documentWords.get(doc1);
+            Set<String> words2 = documentWords.get(doc2);
+
+            Set<String> intersection = new HashSet<>(words1);
+            intersection.retainAll(words2);
+
+            Set<String> union = new HashSet<>(words1);
+            union.addAll(words2);
+
+            double jaccardSimilarity = (double) intersection.size() / union.size();
+            System.out.println("Reducer: Comparing " + doc1 + " and " + doc2 + " -> Similarity = " + jaccardSimilarity); // Debugging log
+
+            if (jaccardSimilarity > 0.5) {
+                context.write(new Text("(" + doc1 + ", " + doc2 + ")"),
+                        new Text(String.format("%.2f%%", jaccardSimilarity * 100)));
+            }
+        }
+    }
+}
 }
